718049d7f1d0b8aa77051709605e408d7c6fd135
Sachin Yadav <sachin@demo.gmail.com>
2024-12-19 16:29:51 +0530

    Update from Sachin

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ import static spark.Spark.*;
  * Main application class.
  */
 public class App {
-    private static final String MESSAGE = "Hello From Hitachi System India";
+    private static final String MESSAGE = "Good Morning";
 
     public static void main(String[] args) {
         // Set the server port
@@ -38,7 +38,7 @@ public class App {
                     </style>
                 </head>
                 <body>
-                    <h1>Hello From Hitachi System India</h1>
+                    <h1>Good Morning</h1>
                 </body>
                 </html>
                 """;
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -11,7 +11,7 @@ public class AppTest {
     @Test
     public void testMessage() {
         App app = new App();
-        assertEquals("Hello From Hitachi System India", "Hello From Hitachi System India");
+        assertEquals("Good Morning", "Good Morning");
     }
 }
 
